cbad16d40eba97381862f51dc88885fb16b28471
smeerws <hello@smeerws.at>
2021-05-30 16:59:32 +0200

    Deklaration eines Arrays namens array vom Datentyp int

--- Assets/MyGame/Scripts/PracticeArrays.cs
+++ Assets/MyGame/Scripts/PracticeArrays.cs
@@ -3,6 +3,10 @@ using UnityEngine.UI;
 
 public class PracticeArrays : MonoBehaviour
 {
+    //Deklaration eines Arrays namens array vom Datentyp int
+    int[] array;
+
+
     private void Start()
     {
         
